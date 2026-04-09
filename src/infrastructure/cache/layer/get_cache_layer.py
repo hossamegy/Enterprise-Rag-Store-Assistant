97e109f1fb6c810b8473dc9ef4fe4a8959cbe581
hossam tammam <hossam.eldingey@gmail.com>
2026-04-09 19:24:37 +0200

    modify cache question

--- src/infrastructure/cache/layer/get_cache_layer.py
+++ src/infrastructure/cache/layer/get_cache_layer.py
@@ -1,18 +1,18 @@
+from src.config.settings import get_nlp_settings
 from src.core.ports.base_layer import BaseLayer
 from src.core.ports.base_cache import BaseCache
 from src.core.entities.message_context import MessageContext
-_CACHE_HIT_THRESHOLD = 0.25
 
 class GetCacheLayer(BaseLayer):
 
     def __init__(self, cache: BaseCache):
         self._cache = cache
-
+         
     def handle(self, context: MessageContext) -> MessageContext:
         if not context.question:
             return context
         score = self._cache.similar_question_score(context.question)
-        if score < _CACHE_HIT_THRESHOLD:
+        if score < get_nlp_settings().confidence_threshold:
             cached_answer = self._cache.get_answer(context.question)
             context.cached_response = cached_answer
             context.response = cached_answer
